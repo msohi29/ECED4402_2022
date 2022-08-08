4fd156407bf2018c55a944ba05a4940499b57a49
adhendricks <36172348+adhendricks@users.noreply.github.com>
2022-08-08 02:59:18 -0300

    Initial Commit

---
+++ Core/Src/User/main_user.c
@@ -0,0 +1,44 @@
+/*
+ * main_user.c
+ *
+ *  Created on: Aug 8, 2022
+ *      Author: Andre Hendricks
+ */
+
+#include <stdio.h>
+
+//STM32 generated header files
+#include "main.h"
+
+//User generated header files
+#include "User/main_user.h"
+#include "User/util.h"
+
+//Required FreeRTOS header files
+#include "FreeRTOS.h"
+#include "task.h"
+
+char main_string[50];
+uint32_t main_counter = 0;
+
+static void main_task(){
+
+	while(1){
+		print_str("Main task loop executing\r\n");
+		sprintf(main_string,"Main task iteration: 0x%08lx\r\n",main_counter++);
+		print_str(main_string);
+		vTaskDelay(1000/portTICK_RATE_MS);
+	}
+}
+
+
+void main_user(){
+	util_init();
+
+	xTaskCreate(main_task,"Main Task", configMINIMAL_STACK_SIZE + 100, NULL, tskIDLE_PRIORITY + 2, NULL);
+
+	vTaskStartScheduler();
+
+	while(1);
+
+}
