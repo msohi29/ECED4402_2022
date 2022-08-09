b9cb9d14aa3c3efda9ccd3480343134835812519
adhendricks <36172348+adhendricks@users.noreply.github.com>
2022-08-09 01:09:14 -0300

    Initial Commit

--- Core/Src/User/main_user.c
+++ Core/Src/User/main_user.c
@@ -21,14 +21,43 @@
 char main_string[50];
 uint32_t main_counter = 0;
 
-static void main_task(void *params){
+static void main_task(void *params)
+{
+	char ibuff[50];
+	TaskHandle_t xHandle[50];
+	/*Variable used to keep track of sub tasks created with button*/
 
-	while(1){
-		print_str("Main task loop executing\r\n");
-		sprintf(main_string,"Main task iteration: 0x%08lx\r\n",main_counter++);
+	size_t val;
+
+	do {
+		print_str("Main task loop executing ");
+
+		// Update hexadecimal 32-bit integer in string, and print it
+		sprintf(main_string,"Main task iteration: 0x%08x\r\n",(unsigned int)main_counter++);
 		print_str(main_string);
-		vTaskDelay(1000/portTICK_RATE_MS);
-	}
+
+		//Create new task if button A was pressed
+		if( /*Check that button A was pressed*/){
+			if (xTaskCreate(&PrintTaskID,(const char*)"subtask",configMINIMAL_STACK_SIZE+100,/*TaskID Parameter*/,tskIDLE_PRIORITY+2,&xHandle[/*fill this*/])!=pdPASS){
+				sprintf(ibuff,"Failed creating new task %d \r\n",/*fill this*/);
+				print_str(ibuff);
+			}else{
+				sprintf(ibuff,"Created a new task %d \r\n",/*fill this*/);
+				print_str(ibuff);
+				/*Increment taskid counter*/
+			}
+			//Delete new task if button B was pressed
+		}else if( /*Check that button B was pressed*/ && /*atleast 1 subtask has been created*/){
+			/*Decrement taskid counter*/
+			vTaskDelete(xHandle[/*fill this*/]);
+			sprintf(ibuff,"Deleted task new task %d \r\n",/*fill this*/);
+			print_str(ibuff);
+		}
+		val = xPortGetFreeHeapSize() ;
+		sprintf(ibuff,"Size = %d Bytes\r\n",val);
+		print_str(ibuff);
+		vTaskDelay(1000 / portTICK_RATE_MS);
+	} while(1);
 }
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
