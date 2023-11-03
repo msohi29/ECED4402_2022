689bc6207bfbb891d32fb17969a96deb5e1b956e
msohi29 <mn961547@dal.ca>
2023-11-03 16:16:48 -0300

    Finished Part 1 of Lab 4.

--- Core/Src/User/main_user.c
+++ Core/Src/User/main_user.c
@@ -18,8 +18,56 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+#include "timers.h"
+
 char main_string[50];
 uint32_t main_counter = 0;
+
+TimerHandle_t xTimers[2];
+enum StopLightStates {STATE_GREEN,STATE_ORANGE,STATE_RED};
+
+void StopLightController( TimerHandle_t xTimer )
+{
+    volatile static enum StopLightStates StopLightController_State = STATE_GREEN;
+
+    uint32_t TimerID;
+    TimerID = ( uint32_t ) pvTimerGetTimerID( xTimer );
+
+    if (StopLightController_State == STATE_GREEN && TimerID == 0){
+        //turn on green light
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_SET); //green
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, GPIO_PIN_RESET); //orange
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_RESET); //red
+        /*Fill: Turn on crossing light (white)*/
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_3, GPIO_PIN_SET); // White
+
+        StopLightController_State = STATE_ORANGE;
+
+    }else if (StopLightController_State == STATE_ORANGE && TimerID == 0){
+
+        /*Fill: Blink crossing light (white) by calling a 2nd software timer*/
+		//turn on orange light
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_RESET); //green
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, GPIO_PIN_SET); //orange
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_RESET); //red
+		StopLightController_State = STATE_RED;
+
+
+    }else if (StopLightController_State == STATE_RED && TimerID == 0){
+        //turn on red light
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_RESET); //green
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, GPIO_PIN_RESET); //orange
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_SET); //red
+        /*Fill: Turn off crossing light (white) and stop the 2nd software timer*/
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_3, GPIO_PIN_RESET); // White
+        StopLightController_State = STATE_GREEN;
+    }
+
+    if(TimerID == 1 && StopLightController_State == STATE_RED) {
+		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_3);
+
+   }
+}
 
 static void main_task(void *param){
 
@@ -33,12 +81,14 @@ static void main_task(void *param){
 
 
 void main_user(){
-	util_init();
+  util_init();
 
-	xTaskCreate(main_task,"Main Task", configMINIMAL_STACK_SIZE + 100, NULL, tskIDLE_PRIORITY + 2, NULL);
+  xTimers[0] = xTimerCreate("Timer1",3000,pdTRUE,( void * ) 0,StopLightController);
+  xTimers[1] = xTimerCreate("Timer2",100,pdTRUE,( void * ) 1,StopLightController);
+  xTimerStart( xTimers[0], 0 );
+  xTimerStart( xTimers[1], 0 );
 
-	vTaskStartScheduler();
-
-	while(1);
-
+  xTaskCreate(main_task,"Task 1", configMINIMAL_STACK_SIZE + 100, NULL, tskIDLE_PRIORITY + 2, NULL);
+  vTaskStartScheduler();
+  while(1);
 }
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -236,12 +236,17 @@ static void MX_USART2_UART_Init(void)
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
@@ -252,6 +257,13 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
+  /*Configure GPIO pins : PC0 PC1 PC2 PC3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
   /*Configure GPIO pin : LD2_Pin */
   GPIO_InitStruct.Pin = LD2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
@@ -259,6 +271,8 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
 
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
